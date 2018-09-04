57a6dddcd8a0372d7538e425c3fbdfb4431fabab
Andrea F. Daniele <afdaniele@ttic.edu>
2018-09-04 17:22:41 -0500

    added callChallengesAPI function

--- AIDO.php
+++ AIDO.php
@@ -11,6 +11,7 @@ use \system\classes\Core;
 use \system\classes\Utils;
 use \system\classes\Database;
 use \system\classes\Configuration;
+use \system\packages\duckietown\Duckietown;
 
 /**
 *   Utility functions for all the AIDO packages
@@ -18,6 +19,9 @@ use \system\classes\Configuration;
 class AIDO{
 
 	private static $initialized = false;
+	private static $challenges_api_protocol = "https";
+	private static $challenges_api_host = "challenges.duckietown.org";
+	private static $challenges_api_version = "v2";
 
 	// disable the constructor
 	private function __construct() {}
@@ -75,7 +79,109 @@ class AIDO{
 	// =======================================================================================================
 	// Public functions
 
-	// YOUR PUBLIC METHODS HERE
+	public static function getSubmissionsStatusList(){
+		return [
+			'submitted',
+			'success',
+			'failed',
+			'evaluating',
+			'retired',
+			'aborted'
+		];
+	}//getSubmissionsStatusList
+
+	public static function getSubmissionsStatusStyle( $status ){
+		$icon = '';
+		$color = '';
+		switch( $status ){
+			case 'submitted':
+				$icon = 'clock-o';
+				$color = 'black';
+				break;
+			case 'success':
+				$icon = 'check';
+				$color = 'green';
+				break;
+			case 'failed':
+				$icon = 'exclamation-circle';
+				$color = 'red';
+				break;
+			case 'evaluating':
+				$icon = 'refresh';
+				$color = '#337ab7';
+				break;
+			case 'retired':
+				$icon = 'trash';
+				$color = 'black';
+				break;
+			case 'aborted':
+				$icon = 'hand-paper-o';
+				$color = 'red';
+				break;
+			default: break;
+		}
+		return ['icon' => $icon, 'color' => $color];
+	}//getSubmissionsStatusStyle
+
+	public static function callChallengesAPI( $method, $service, $action=null, $data=[], $headers=[], $user_id=null ){
+		if( !in_array($method, ['GET', 'POST', 'DELETE']) )
+			return ['success'=>false, 'data'=>sprintf('Method `%s` not supported', $method)];
+		// get duckietoken
+		$token = Duckietown::getUserToken();
+		if( !is_null($user_id) ){
+			$token = Duckietown::getUserToken($user_id);
+		}
+		// build querystring
+		$querystring = '';
+		if( $method == 'GET' )
+			$querystring = toQueryString( array_keys($data), $data, true/*questionMarkAppend*/ );
+		// build url
+		$url = sprintf(
+			'%s://%s/%s/%s%s%s',
+			self::$challenges_api_protocol,
+			self::$challenges_api_host,
+			self::$challenges_api_version,
+			$service,
+			is_null($action)? '' : sprintf('/%s', $action),
+			$querystring
+		);
+		// configure a CURL object
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+		if( $method == 'POST' )
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_data));
+		if( !is_null($token) )
+			array_push($headers, sprintf('X-Messaging-Token: %s', $token));
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		// call CURL
+		$curl_response = curl_exec($curl);
+		$curl_res = curl_getinfo($curl);
+		curl_close($curl);
+		// handle errors
+		if( $curl_response === false || $curl_res['http_code'] != 200 ){
+			return [
+				'success'=>false,
+				'data'=>sprintf(
+					'An error occurred while talking to the challenges API. The server returned the code <strong>%d</strong>.',
+					$curl_res['http_code']
+				)
+			];
+		}
+		// get answer
+		$decoded = json_decode($curl_response, true);
+		if( isset($decoded['ok']) && $decoded['ok'] === false ){
+			return [
+				'success'=>false,
+				'data'=>sprintf(
+					'An error occurred while talking to the challenges API. The server reports: "%s"',
+					json_encode($decoded['result'])
+				)
+			];
+		}
+		// success
+		return [ 'success'=>true, 'data'=>$decoded['result'] ];
+	}//callChallengesAPI
 
 
 
